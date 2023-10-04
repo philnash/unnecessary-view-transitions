ab3192438f74dc87409d8e2a91dcdf899a743208
Phil Nash <philnash@gmail.com>
2023-10-04 17:11:02 +1100

    Initial set of page transitions

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(bundlerPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy("src/assets/base.css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  }
+};
