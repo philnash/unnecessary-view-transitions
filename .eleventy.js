aa7b1ff13652aed65a7eed71148b47cbbc11b9f6
Phil Nash <philnash@gmail.com>
2023-11-15 15:34:49 +0000

    Fix paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,16 @@
 const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPlugin(bundlerPlugin);
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(bundlerPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("src/assets/base.css");
 
   return {
     dir: {
       input: "src",
-      output: "_site"
-    }
-  }
+      output: "_site",
+    },
+    pathPrefix: "/unnecessary-view-transitions/",
+  };
 };
